44c055626fd9d08ac3d65537fd67f0242d3a59e3
Damilola Abioye <35523688+damyl4sure@users.noreply.github.com>
2021-10-18 14:40:09 +0100

    Update .js

--- .js
+++ .js
@@ -2,4 +2,4 @@ let length = 16;                               // Number
 let lastName = "Johnson";                      // String
 let x = {firstName:"John", lastName:"Doe"};    // Object
 
-let y = {firstName:"Mons", lastName:"Test"};    // Object
+let y = {firstName:"Tues", lastName:"Test"};    // Object
